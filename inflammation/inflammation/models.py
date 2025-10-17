42b34c2a81bc2478a40dc7ca0d8acef46c037416
moritzhauschulz <moritz.hauschulz@gmail.com>
2025-10-17 13:56:53 +0100

    added test case for value error

--- inflammation/inflammation/models.py
+++ inflammation/inflammation/models.py
@@ -49,6 +49,8 @@ def patient_normalise(data):
     Negative values are rounded to 0.
     """
     assert min(data.shape) > 0, 'Data must not be empty'
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
     max = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
         normalised = data / max[:, np.newaxis]
